5251a80a71fd03734925b9d9874972ccfa9c5bfd
Darcy Liu <d273liu@uwaterloo.ca>
2023-10-31 12:06:15 -0400

    Make commit ON Tue Oct 31 12:06:15 EDT 2023

--- clockserver.c
+++ clockserver.c
@@ -9,7 +9,7 @@
 #define CLOCKINTID 99
 void clock_notifier(){
     int clock_server_tid = Create(0, clock_server);
-    uart_printf(CONSOLE, "clock_server: tid = %d\r\n", tid);
+    // uart_printf(CONSOLE, "clock_server: tid = %d\r\n", tid);
     RegisterAs("clock_notifier");
     // uart_printf(CONSOLE, "clock_notifier:Registered\n");
     while (1)
