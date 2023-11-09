698dda4d08167ce94d5f568bd7db8f9056bef9f7
Darcy Liu <d273liu@uwaterloo.ca>
2023-11-09 07:05:40 -0500

    stop at not working

---
+++ tc1/goto.h
@@ -0,0 +1,17 @@
+#include "track_server.h"
+#include "marklin_worker.h"
+#include "train_control.h"
+#include "track_data_new.h"
+#include <stdint.h>
+/*
+Track node.c
+typedef enum {
+  NODE_NONE,
+  NODE_SENSOR,
+  NODE_BRANCH,
+  NODE_MERGE,
+  NODE_ENTER,
+  NODE_EXIT,
+} node_type;
+*/
+void path_switch(char* start_str, char* end_str);
